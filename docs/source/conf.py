297b114c6773676ddab6a943735849c1085e690f
Christophe COSSOU <christophe.cossou@cea.fr>
2022-12-06 18:32:15 +0100

    RST: minor

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Miritools'
+copyright = '2022, Christophe Cossou'
+author = 'Cossou'
 
 release = '0.1'
 version = '0.1.0'
